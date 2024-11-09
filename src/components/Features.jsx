0c7f6edf13571067da5b91e0390ac3c1fbc38727
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 16:56:56 +0530

    Create heading.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,9 +3,14 @@ import "./Features.css";
 function Features() {
   return (
     <>
-    <div className="text-center w-75"><h2>Motivated Mornings is a virtual coworking community proven to help you do the work.</h2></div>
+      <div className="text-center w-75">
+        <h2>
+          Motivated Mornings is a virtual coworking community proven to help you
+          do the work.
+        </h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default Features
+export default Features;
