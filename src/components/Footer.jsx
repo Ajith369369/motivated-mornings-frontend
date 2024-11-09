c9f50ef84c91fc1ac1242b6ecb163db3ac161b22
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 05:19:09 +0530

    Create footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
 import "./Footer.css";
 
 function Footer() {
   return (
-    <>Footer</>
+    <>
+      <section id="footer" className="bg-dark">
+        <h3 className="text-light">Questions? Let's Talk</h3>
+        <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
+        <button className="btn rounded-pill bg-warning">Let's Talk</button>
+        <div></div>
+        <div>
+          <p>Email: hello@motivatedmornings.work</p>
+          <p>© 2020 Motivated Mornings, Inc. All rights reserved. Site by Knapsack.</p>
+          <p>guidelines privacy terms members</p>
+          <div>
+          <button className="btn rounded-pill bg-warning">Help</button>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
