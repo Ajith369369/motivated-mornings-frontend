d90d164bc7f445f617117b5ab73eb3ec1de3f3db
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 05:25:55 +0530

    Arrange items in column.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ function Footer() {
   return (
     <>
       <section id="footer" className="bg-dark mt-5 py-5">
-        <div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
           <h3 className="text-light">Questions? Let's Talk</h3>
           <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
           <button className="btn rounded-pill bg-warning">Let's Talk</button>
           <div></div>
           <div>
-            <p className="text-light">Email: hello@motivatedmornings.work</p>
-            <p className="text-light">© 2020 Motivated Mornings, Inc. All rights reserved. Site by Knapsack.</p>
-            <p className="text-light">guidelines privacy terms members</p>
+            <p className="text-light text-center mb-1">Email: hello@motivatedmornings.work</p>
+            <p className="text-light text-center mb-1">© 2020 Motivated Mornings, Inc. All rights reserved. Site by Knapsack.</p>
+            <p className="text-light text-center mb-1">guidelines privacy terms members</p>
         </div>
           <div>
           <button className="btn rounded-pill bg-warning">Help</button>
