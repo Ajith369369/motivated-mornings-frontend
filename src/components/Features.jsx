ed75a264ad1c234e6d91464108aa7e3e81b8d234
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 17:22:26 +0530

    Add bottom margin.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,7 @@ import "./Features.css";
 function Features() {
   return (
     <>
-      <div className="row">
+      <div className="row mb-5">
         <div className="col-md-1"></div>
         <div className="col-md-10">
           <div className="text-center">
@@ -58,7 +58,7 @@ function Features() {
               </p>
             </div>
           </div>
-          <p className="m-0 text-center">
+          <p className="m-0 mb-4 text-center">
             *Available exclusively with the Pro plan
           </p>
         </div>
