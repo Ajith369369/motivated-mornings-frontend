ed35e9600fb3d46aa3bdbc0f8a9fa695c6120d32
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 13:44:16 +0530

    Create sections.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,36 @@ import "./Header.css";
 function Header() {
   return (
     <>
-    <section id="navbar"></section>
-    <section id="actually"></section>
-    <section id="tired"></section>
+      <section id="header">
+        <section id="navbar"></section>
+        <section id="actually">
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-5"></div>
+            <div className="col-md-5"></div>
+            <div className="col-md-1"></div>
+          </div>
+        </section>
+        <section id="tired">
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-5"></div>
+            <div className="col-md-5"></div>
+            <div className="col-md-1"></div>
+          </div>
+        </section>
+        <section id="better">
+          <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-4"></div>
+            <div className="col-md-2"></div>
+            <div className="col-md-4"></div>
+            <div className="col-md-1"></div>
+          </div>
+        </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
