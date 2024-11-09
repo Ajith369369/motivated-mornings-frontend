3e03c2d6c6f4ac329715fa6f5d52c47fb450faba
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 05:22:20 +0530

    Create div.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,16 @@ function Footer() {
   return (
     <>
       <section id="footer" className="bg-dark mt-5 py-5">
-        <h3 className="text-light">Questions? Let's Talk</h3>
-        <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
-        <button className="btn rounded-pill bg-warning">Let's Talk</button>
-        <div></div>
         <div>
-          <p>Email: hello@motivatedmornings.work</p>
-          <p>© 2020 Motivated Mornings, Inc. All rights reserved. Site by Knapsack.</p>
-          <p>guidelines privacy terms members</p>
+          <h3 className="text-light">Questions? Let's Talk</h3>
+          <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
+          <button className="btn rounded-pill bg-warning">Let's Talk</button>
+          <div></div>
+          <div>
+            <p>Email: hello@motivatedmornings.work</p>
+            <p>© 2020 Motivated Mornings, Inc. All rights reserved. Site by Knapsack.</p>
+            <p>guidelines privacy terms members</p>
+        </div>
           <div>
           <button className="btn rounded-pill bg-warning">Help</button>
           </div>
