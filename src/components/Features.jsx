f18b0f309578f6434c509feca80e467232bcd9a7
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 17:03:47 +0530

    Create flex items.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,6 +12,16 @@ function Features() {
               you do the work.
             </h2>
           </div>
+          <div>
+            <div>
+              <img src="/src/assets/morning.png" alt="" />
+              <h4>Hi</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Laboriosam quibusdam quae blanditiis ad eius repellendus.
+              </p>
+            </div>
+          </div>
         </div>
         <div className="col-md-1"></div>
       </div>
