8594c0e53f4244343655a4ba0d210db98d1d8dfa
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 16:01:57 +0530

    Create waveform.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,6 +30,18 @@ function Header() {
             <div className="col-md-1"></div>
           </div>
         </section>
+        {/* <svg viewBox="0 0 500 200">
+          <path
+            d="M 0 30 C 150 100 280 0 500 20 L 500 0 L 0 0"
+            fill="#edc444"
+          ></path>
+        </svg> */}
+        <svg viewBox="0 0 500 800">
+          <path
+            d="M 0 250 C 300 420 250 90 500 100 L 500 0 L 0 0"
+            fill="#edc444"
+          ></path>
+        </svg>
       </section>
     </>
   );
