b860b514cb528c926a6405460bbaa78c966ffa9b
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 05:21:50 +0530

    Change footer section style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <section id="footer" className="bg-dark">
+      <section id="footer" className="bg-dark mt-5 py-5">
         <h3 className="text-light">Questions? Let's Talk</h3>
         <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
         <button className="btn rounded-pill bg-warning">Let's Talk</button>
