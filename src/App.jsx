92943e76a2a45d22b2053a67613a48689565ccca
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 13:14:21 +0530

    Add components to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Pricing from './components/Pricing'
 
 function App() {
   
 
   return (
     <>
-      
+      <Header/>
+      <Features/>
+      <Pricing/>
+      <Footer/>
     </>
   )
 }
