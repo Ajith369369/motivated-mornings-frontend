7ab991975bbb0fcb7cff05ec4b2b1741cc02e8d4
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 05:28:10 +0530

    Change style of paragraph.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <section id="footer" className="bg-dark mt-5 py-5">
         <div className="d-flex flex-column justify-content-center align-items-center">
           <h3 className="text-light">Questions? Let's Talk</h3>
-          <p className="text-light">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
+          <p className="text-light w-50 text-center">We'll help you determine if Motivated Mornings is the right fit for you and answer any of your questions.</p>
           <button className="btn rounded-pill bg-warning">Let's Talk</button>
           <div></div>
           <div>
