b5cb0d6a91fca57f2d763427975d9477d7e48eaf
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 21:10:08 +0530

    Create sections.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from "react";
 import "./Pricing.css";
 
 function Pricing() {
   return (
-    <>Pricing</>
-  )
+    <>
+      <section id="trial">
+        <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-8">
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="d-flex justify-content-center align-items-center">
+              <button className="btn rounded-pill">View Plans Details</button>
+            </div>
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+      </section>
+      <section id="complete">
+        <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-8">
+            <img src="/src/assets/actually-complete.png" alt="" />
+            <div></div>
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+      </section>
+      <section id="secrets">
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-5"></div>
+          <div className="col-md-5">
+            <img src="/src/assets/8-secrets.png" alt="" />
+          </div>
+          <div className="col-md-1"></div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Pricing
+export default Pricing;
