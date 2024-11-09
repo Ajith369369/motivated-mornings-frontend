28376650d6c034cedfe721f80c975e2e7dcd6711
Ajith K. S. <ajithks98086@gmail.com>
2024-11-09 17:00:16 +0530

    Create rows & columns.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,11 +3,17 @@ import "./Features.css";
 function Features() {
   return (
     <>
-      <div className="text-center w-75">
-        <h2>
-          Motivated Mornings is a virtual coworking community proven to help you
-          do the work.
-        </h2>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-10">
+          <div className="text-center">
+            <h2>
+              Motivated Mornings is a virtual coworking community proven to help
+              you do the work.
+            </h2>
+          </div>
+        </div>
+        <div className="col-md-1"></div>
       </div>
     </>
   );
