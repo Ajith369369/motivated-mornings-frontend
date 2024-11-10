c147d93d3a9a48a7a444614bdb2a53ec73edbd3d
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 11:38:51 +0530

    Create features section.

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,67 +3,69 @@ import "./Features.css";
 function Features() {
   return (
     <>
-      <div className="row mb-5">
-        <div className="col-md-1"></div>
-        <div className="col-md-10">
-          <div className="text-center">
-            <h2>
-              Motivated Mornings is a virtual coworking community proven to help
-              you do the work.
-            </h2>
+      <section id="features" className="my-5">
+        <div className="row mb-5">
+          <div className="col-md-1"></div>
+          <div className="col-md-10">
+            <div className="text-center">
+              <h2>
+                Motivated Mornings is a virtual coworking community proven to help
+                you do the work.
+              </h2>
+            </div>
+            <div className="d-flex flex-wrap justify-content-center align-items-center mb-4">
+              <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
+                <img className="flex-img" src="/src/assets/morning.png" alt="" />
+                <h4 className="my-2">Hi</h4>
+                <p className="m-0 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam quibusdam quae blanditiis ad eius repellendus.
+                </p>
+              </div>
+              <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
+                <img className="flex-img" src="/src/assets/virtual.png" alt="" />
+                <h4 className="my-2">Hi</h4>
+                <p className="m-0 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam quibusdam quae blanditiis ad eius repellendus.
+                </p>
+              </div>
+              <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
+                <img className="flex-img" src="/src/assets/private.png" alt="" />
+                <h4 className="my-2">Hi</h4>
+                <p className="m-0 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam quibusdam quae blanditiis ad eius repellendus.
+                </p>
+              </div>
+              <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
+                <img
+                  className="flex-img"
+                  src="/src/assets/productivity.png"
+                  alt=""
+                />
+                <h4 className="my-2">Hi</h4>
+                <p className="m-0 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam quibusdam quae blanditiis ad eius repellendus.
+                </p>
+              </div>
+              <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
+                <img className="flex-img" src="/src/assets/tailored.png" alt="" />
+                <h4 className="my-2">Hi</h4>
+                <p className="m-0 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Laboriosam quibusdam quae blanditiis ad eius repellendus.
+                </p>
+              </div>
+            </div>
+            <p className="m-0 mb-4 text-center">
+              *Available exclusively with the Pro plan
+            </p>
           </div>
-          <div className="d-flex flex-wrap justify-content-center align-items-center mb-4">
-            <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
-              <img className="flex-img" src="/src/assets/morning.png" alt="" />
-              <h4 className="my-2">Hi</h4>
-              <p className="m-0 text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Laboriosam quibusdam quae blanditiis ad eius repellendus.
-              </p>
-            </div>
-            <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
-              <img className="flex-img" src="/src/assets/virtual.png" alt="" />
-              <h4 className="my-2">Hi</h4>
-              <p className="m-0 text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Laboriosam quibusdam quae blanditiis ad eius repellendus.
-              </p>
-            </div>
-            <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
-              <img className="flex-img" src="/src/assets/private.png" alt="" />
-              <h4 className="my-2">Hi</h4>
-              <p className="m-0 text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Laboriosam quibusdam quae blanditiis ad eius repellendus.
-              </p>
-            </div>
-            <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
-              <img
-                className="flex-img"
-                src="/src/assets/productivity.png"
-                alt=""
-              />
-              <h4 className="my-2">Hi</h4>
-              <p className="m-0 text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Laboriosam quibusdam quae blanditiis ad eius repellendus.
-              </p>
-            </div>
-            <div className="flex-items d-flex flex-column justify-content-center align-items-center m-3">
-              <img className="flex-img" src="/src/assets/tailored.png" alt="" />
-              <h4 className="my-2">Hi</h4>
-              <p className="m-0 text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Laboriosam quibusdam quae blanditiis ad eius repellendus.
-              </p>
-            </div>
-          </div>
-          <p className="m-0 mb-4 text-center">
-            *Available exclusively with the Pro plan
-          </p>
+          <div className="col-md-1"></div>
         </div>
-        <div className="col-md-1"></div>
-      </div>
+      </section>
     </>
   );
 }
