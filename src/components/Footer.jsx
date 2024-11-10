c67d1cff67094278537526f6d119f08e4ed023e8
Ajith K. S. <ajithks98086@gmail.com>
2024-11-10 13:19:05 +0530

    Add icons.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Footer.css";
 
 function Footer() {
@@ -7,31 +13,33 @@ function Footer() {
         <div className="row">
           <div className="col-md-2"></div>
           <div className="col-md-8 d-flex flex-column justify-content-center align-items-center">
-          <h3 className="text-light my-3">Questions? Let's Talk</h3>
-          <p className="text-light w-50 text-center my-4">
-            We'll help you determine if Motivated Mornings is the right fit for
-            you and answer any of your questions.
-          </p>
-          <button className="btn rounded-pill bg-warning p-4">Let's Talk</button>
-          <div>
-            
-          </div>
-          <div>
-            <p className="text-light text-center mb-1">
-              Email: hello@motivatedmornings.work
+            <h3 className="text-light my-3">Questions? Let's Talk</h3>
+            <p className="text-light w-50 text-center my-4">
+              We'll help you determine if Motivated Mornings is the right fit
+              for you and answer any of your questions.
             </p>
-            <p className="text-light text-center mb-1">
-              © 2020 Motivated Mornings, Inc. All rights reserved. Site by
-              Knapsack.
-            </p>
-            <p className="text-light text-center mb-1">
-              guidelines privacy terms members
-            </p>
-          </div>
+            <button className="btn rounded-pill bg-warning p-4">
+              Let's Talk
+            </button>
+            <div className="d-flex flex-wrap justify-content-center align-items-center my-4">
+              <FontAwesomeIcon icon={faInstagram} className="text-light" />
+              <FontAwesomeIcon icon={faFacebookF} className="text-light" />
+              <FontAwesomeIcon icon={faLinkedin} className="text-light" />
+            </div>
+            <div>
+              <p className="text-light text-center mb-1">
+                Email: hello@motivatedmornings.work
+              </p>
+              <p className="text-light text-center mb-1">
+                © 2020 Motivated Mornings, Inc. All rights reserved. Site by
+                Knapsack.
+              </p>
+              <p className="text-light text-center mb-1">
+                guidelines privacy terms members
+              </p>
+            </div>
           </div>
           <div className="col-md-2"></div>
-
-          
         </div>
         <div className="d-flex justify-content-end align-items-center">
           <button className="btn rounded-pill bg-warning me-4">Help</button>
